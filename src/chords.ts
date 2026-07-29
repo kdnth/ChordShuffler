89f53f18315fbde826afc0862590c1bb42ede556
kdnth <kadenconradt@gmail.com>
2026-07-29 15:06:55 -0400

    refactor: change `Root` to `Note` and change diminished symbol from `o` to `º`

--- src/chords.ts
+++ src/chords.ts
@@ -1,32 +1,32 @@
-export enum Root {
-    A = 'A',
-    A_FLAT = 'Ab',
-    A_SHARP = 'A#',
-    B = 'B',
-    B_FLAT = 'Bb',
-    C = 'C',
-    C_SHARP = 'C#',
-    D = 'D',
-    D_FLAT = 'Db',
-    D_SHARP = 'D#',
-    E = 'E',
-    E_FLAT = 'Eb',
-    F = 'F',
-    F_SHARP = 'F#',
-    G = 'G',
-    G_FLAT = 'Gb',
-    G_SHARP = 'G#'
+export enum Note {
+  A = 'A',
+  A_FLAT = 'Ab',
+  A_SHARP = 'A#',
+  B = 'B',
+  B_FLAT = 'Bb',
+  C = 'C',
+  C_SHARP = 'C#',
+  D = 'D',
+  D_FLAT = 'Db',
+  D_SHARP = 'D#',
+  E = 'E',
+  E_FLAT = 'Eb',
+  F = 'F',
+  F_SHARP = 'F#',
+  G = 'G',
+  G_FLAT = 'Gb',
+  G_SHARP = 'G#'
 }
 
 export enum Quality {
-    MAJ_7 = "maj7",
-    MIN_7 = "min7",
-    DOM_7 = "7",
-    HALF_DIM = "ø",
-    DIM_7 = "o"
+  MAJ_7 = 'maj7',
+  MIN_7 = 'min7',
+  DOM_7 = '7',
+  HALF_DIM = 'ø',
+  DIM_7 = 'º'
 }
 
 export interface Chord {
-    root: Root
-    quality: Quality
+  note: Note;
+  quality: Quality;
 }
