8f0b2804aa60444db553f5c65b55968d356499af
kdnth <kadenconradt@gmail.com>
2026-07-29 15:40:15 -0400

    chore: add MIT license and attribution

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
+import AppFooter from './components/AppFooter.vue';
 import HomeView from './views/HomeView.vue';
 </script>
 
 <template>
   <HomeView />
+  <AppFooter class="absolute bottom-2 left-1/2 -translate-x-1/2" />
 </template>
 
 <style scoped></style>
---
+++ src/components/AppFooter.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <p class="hover:underline hover:text-yellow-500">
+      Chord Shuffler by <a href="https://github.com/kdnth">kdnth</a>
+    </p>
+  </div>
+</template>
