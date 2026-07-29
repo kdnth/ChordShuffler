05d6646c4dc3bbff047c1c2cdfb698648144487a
kdnth <kadenconradt@gmail.com>
2026-07-29 15:27:13 -0400

    feat: Add hero for home view

---
+++ src/components/HomeHero.vue
@@ -0,0 +1,6 @@
+<script lang="ts"></script>
+<template>
+  <div class="w-full items-end flex min-h-48 p-10 bg-purple-800 text-white">
+    <h1 class="text-6xl font-semibold">Chord Shuffler</h1>
+  </div>
+</template>
