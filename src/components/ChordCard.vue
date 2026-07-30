ccb4fd33e0ebd128a66ea43b175a8df20eea5f0d
kdnth <kadenconradt@gmail.com>
2026-07-30 00:02:49 -0400

    style: add shadow + border to chordcard

--- src/components/ChordCard.vue
+++ src/components/ChordCard.vue
@@ -6,7 +6,9 @@ const props = defineProps<{
 }>();
 </script>
 <template>
-  <div class="rounded-xl min-h-48 bg-blue-500 flex items-center justify-center shadow-xl">
+  <div
+    class="rounded-xl min-h-48 bg-blue-500 flex items-center justify-center shadow-sm shadow-neutral-500 border border-blue-300"
+  >
     <div v-if="props.chord">
       <h1 class="text-5xl text-white font-black tracking-wide font-body">
         {{ props.chord?.note }}{{ props.chord?.quality }}
